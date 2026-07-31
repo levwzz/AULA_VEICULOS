c715ebc77c919fc26fcce7e0acf31f2467bdb2c2
Levi Silveira <levileitedasilveira@gmail.com>
2026-07-30 22:43:56 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+void main() {
+    veiculo ferrari = new veiculo("vermelho", "ferrari", "SF90");
+
+    veiculo tcross = new veiculo("Azul", "Volkswagen", "T-cross highline");
+
+    veiculo Lamborghini = new veiculo("Azul shark", "Porsche", "911 GT3 RS");
+
+    veiculo fiat = new veiculo("Vermelho pop", "Fiat", "Uno");
+
+    IO.println(ferrari.acelerar());
+    IO.println(tcross.acelerar());
+
+}
---
+++ veiculo.java
@@ -0,0 +1,30 @@
+import static java.lang.IO.*;
+
+public class veiculo {
+    public String cor;
+    public String marca;
+    public String modelo;
+
+    public String acelerar(){
+        return "Vruuuuummmmm";
+    }
+
+    public String freiar(){
+        return "iriiiiiiiii";
+    }
+
+    public veiculo(String cor, String marca, String modelo) {
+        this.cor = cor;
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+
+    @Override
+    public String toString() {
+        return "veiculo{" +
+                "cor='" + cor + '\'' +
+                ", marca='" + marca + '\'' +
+                ", modelo='" + modelo + '\'' +
+                '}';
+    }
+}
